abb1e9257a40e586615c24fa1b09d5ea17b40674
Shirley Kaffrey <jazfrey@gmail.com>
2020-09-18 12:28:09 -0600

    fixed spelling errors and added to who

--- src/components/who.jsx
+++ src/components/who.jsx
@@ -15,7 +15,7 @@ export default class who extends Component {
             </blockquote>
                   
             
-                 <p>I am a life long learning and leader that enjoys the beautiful mountains of Colorado. I started my career life as
+                 <p>I am a lifelong learner and leader that enjoys the beautiful mountains of Colorado. I enjoy educating people on health and staying active.  I started my life's adventure as
                   a Physical Therapist Assistant and then got promoted to a Rehab Manager. With the world changing and my therapist
                   friends wanting technology to make their testing and documentation easier and quicker, I soon realized, I could do
                   that! I returned to school to learn coding to help people in a different way. I finished General Assembly's
