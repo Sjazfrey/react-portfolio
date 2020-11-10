b6bcaf5ad5a0a1db771b287b67bc1606ef9d7183
Shirley Kaffrey <jazfrey@gmail.com>
2020-11-10 12:54:38 -0700

    changed resume

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -8,69 +8,9 @@
               <div id="resume">   
       
       <iframe src="./Resume.pdf" width="100%" height="600px"></iframe>
-        {/* <h2>Resume</h2>
-        <b>Shirley Kaffrey<br />Software Developer<br /> Loveland, CO<br /> 719.238.9967<br /></b>
-           <a href="mailto:Shirley.Kaffrey@gmail.com​">Shirley.Kaffrey@gmail.com​</a>| jazfrey.github<br />
-    
-          <br />Self starter, life-long learner and adaptable leader turned software developer. I am an unshakable optimist
-          waiting for a challenge to work and expand my web-based software proficiency.<br />
-    
-          <br /><strong>SKILLS</strong>
-          <br />
-          Full Stack Development, HTML, CSS, JavaScript, jQuery, GitHub, Git command line interface, NodeJS, Express,
-          MongoDB / Mongoose, CRUD, React, Heroku.
-          <br />As a Rehab Manager for 5 year, July 2012-March 2020, my skills included, meeting budget and productivity
-          standards for myself and employees, building relationships and caseload, directing growth and development of the
-          rehab department, interpersonal communication with clients, their families and physicians along with management,
-          and maintaining supplies and organization of the Rehab department.<br />
-    
-          <br />
-          General Assembly, ​Atlanta, GA<br /> 05/2020 - 08/2020<br />
-          <br /><strong>Software Engineering Immersive Program:</strong><br />
-          Applied skills in CRUD apps, GitHub, DOM manipulation, AJAX, CSS, jQuery, Media Queries, Express, 7 RESTful
-          routes, Mongo and Mongoose, Sessions and Authentication, along with Heroku in a 500-hour full-time course over the
-          duration of only 3 months.<br />
-          <strong>PROJECTS:</strong><br />
-           <i>* ​Developed a portfolio of projects:</i>< br />
-          <strong>Memory Game​ Project:</strong> Using GitHub, DOM, AJAX, CSS, jQuery. This app was developed using
-          jquery animate along with the above mentioned technologies. It's a tile base game with an 8x8 grid where the user
-          turns over 2 tiles at a time to attempt to find a match. If the 2 tiles do not match then the tiles will flip back
-          over and the user can take another turn. ​<br /><br />
-          <strong>Tinetti Balance Website​- Second Project:</strong> CRUD app using GitHub, DOM manipulation, AJAX, CSS,
-          jQuery, Express, EJS, 7 RESTful routes, MongoDB / Mongoose, and Heroku. This app was developed for Physical
-          Therapists to use to be able to perform the balance test with their clients. This project used radio buttons to
-          click on to score each test section then automatically scored the test for the therapist, and it would keep the
-          score, the date, and the name which assisted the therapist in keeping track of this information.
-          <br /><br />
-    
-          <strong>Bookworm-​ Third project was a group project.</strong> I did the frontend using MERN: MongoDB,
-          Express, React, and NodeJS along with CRUD. This app was developed using Google API to search for books, favorite
-          books, and add books to your collection of books. <br />
-    
-          <br /><strong>Smartini App-Fourth project.</strong>This Trivia App used Express, Handlebars and Zurb Foundations along with Socket.io to create this app to make an online trivia game that can use Zoom or another online video chat to answer trivia questions and then submit them to the host of the game. <br /><br />
-          
-    
-          <strong>EXPERIENCE:</strong>
-          <br />Legacy Healthcare​, Longmont, CO | Rehab manager/PTA <br />04/2014 - 03/2020<br />
-    
-          My responsibilities as a Rehab Manager, included yearly performance reviews of the employees, interviewing
-          potential employees, directing the growth and development of the rehab program, performing tasks to meet the
-          fiscal budget requirements, maintaining professional working relations with the patients, their families,
-          physicians, building management and vendors and ordering supplies necessary for the function of the department.
-          Point of service documentation to maintain and increase productivity standards.
-          Licensed Physical Therapist Assistant: In the field for 15+ years, worked in various settings providing patient
-          care to increase strength, balance, and rehab for total knee, hip and shoulders. Skills include caring and
-          positive attitude, working and motivating people, point of service documentation on electronic device to increase
-          productivity.<br /><br />
-    
-          <strong>EDUCATION:</strong>
-          
-          General Assembly Software Engineering Immersive, Atlanta, GA 05/2020 - 08/2020
         
-          Associate in Applied
-          Science in PTA, Colby Community College, Colby, Ks. */}
     
-          </div>    
+              </div>    
         
               
             
